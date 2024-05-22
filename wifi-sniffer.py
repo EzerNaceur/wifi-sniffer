b551b2148ba6072e2ed69de657c53a48effec0a5
EzerNaceur <azernaceur17@gmail.com>
2024-05-22 16:29:07 +0100

    Created the wifi-sniffer program

---
+++ wifi-sniffer.py
@@ -0,0 +1,23 @@
+from scapy.all import *
+import sys
+interface = ''
+probeReqs = []
+
+def sniffProves(p):
+
+	if p.haslayer(Dot11ProbeReq):
+		netName = p.getlayer(Dot11ProbReq).info
+		if netName not in probeReqs:
+			probeReqs.append(netName)
+			print('[+] Detected New Probe Request' + netName)
+
+if __name__ == "__main__":
+	if len(sys.argv) >= 2:
+		interface = sys.argv[1]
+		print('Sniffing' + interface + 'for any probe requests: \n')
+		sniff(iface=interface, prn=sniffProves)
+	else: 
+		print("No interface provided. Provide an interface to start sniffing")
+	
+		
+
